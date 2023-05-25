a117d10ba6abf8348c3d419d394c579440bdb168
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2023-05-25 11:07:54 +0900

    Update gsi.php

--- gsi.php
+++ gsi.php
@@ -12,11 +12,12 @@ if (mb_strtolower($_SERVER ['REQUEST_METHOD']) != 'post' ) {
 if ( !isset($_POST) || !is_array($_POST) ) {
     http_response_code(400);
     $curl_res['ts']   = time();
-    $curl_res['mesg'] = 'Bad params.';
+    $curl_res['mesg'] = 'Bad request.';
 
     die(json_encode($curl_res));
 }
 
 define('HTTP_X_USER_ID', json_decode($_SERVER['HTTP_X_USER_ID'], true));
+
 
 var_dump([$_REQUEST,$_SERVER,HTTP_X_USER_ID]);
