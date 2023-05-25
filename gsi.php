970ad24b49dfb11adfc81b3c826775c21aacd566
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2023-05-25 11:05:16 +0900

    Update gsi.php

--- gsi.php
+++ gsi.php
@@ -1,2 +1,22 @@
 <?php session_start();
-var_dump([$_REQUEST,$_SERVER]);
+require_once './vendor/autoload.php';
+
+if (mb_strtolower($_SERVER ['REQUEST_METHOD']) != 'post' ) {
+    http_response_code(405);
+    $curl_res['ts']   = time();
+    $curl_res['mesg'] = 'Method Not Allowed.';
+
+    die(json_encode($curl_res));
+}
+
+if ( !isset($_POST) || !is_array($_POST) ) {
+    http_response_code(400);
+    $curl_res['ts']   = time();
+    $curl_res['mesg'] = 'Bad params.';
+
+    die(json_encode($curl_res));
+}
+
+define('HTTP_X_USER_ID', json_decode($_SERVER['HTTP_X_USER_ID'], true));
+
+var_dump([$_REQUEST,$_SERVER,HTTP_X_USER_ID]);
