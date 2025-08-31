3c88eeb28951ed6ab571a2c514bc25d1fa8d6ab5
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2025-08-31 17:11:01 +0900

    CORS

--- repo2/server/index.php
+++ repo2/server/index.php
@@ -3,4 +3,8 @@ session_start();
 
 date_default_timezone_set('Asia/Tokyo');
 header('Content-Type: application/json; charset=UTF-8');
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Content-Length, Origin, Accept, Access-Control-Allow-Headers, X-Token");
+
 
