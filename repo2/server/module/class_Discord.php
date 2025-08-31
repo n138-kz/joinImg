c5e69ed3cdcb46514a06f0960371c5b6e5029626
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2025-08-31 23:45:19 +0900

    function getUserInfo

--- repo2/server/module/class_Discord.php
+++ repo2/server/module/class_Discord.php
@@ -7,4 +7,29 @@ class Discord {
 		}
 		return $this->getUserInfo($qlist['access_token']);
 	}
+	function getUserInfo($token) {
+		$url = 'https://discordapp.com/api/users/@me';
+		$header = [
+			'Authorization: Bearer '.$token,
+		];
+		$req = curl_init();
+		curl_setopt($req, CURLOPT_URL, $url);
+		curl_setopt($req, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($req, CURLOPT_HTTPHEADER, $header);
+		$res = curl_exec($req);
+		$res = json_decode($res, true);
+		$res = array_merge([
+			'id'=>'',
+			'username'=>'',
+			'avatar'=>'',
+			'global_name'=>'',
+			'mfa_enabled'=>false,
+			'locale'=>'',
+		], $res);
+		$res_info = curl_getinfo($req);
+		return [
+			$res,
+			$res_info,
+		];
+	}
 }
